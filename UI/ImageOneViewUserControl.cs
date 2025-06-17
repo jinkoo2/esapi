7fcaae828ac223bf97c08fff69cc98b90aec8f39
Jinkoo <jinkoo2@hotmail.com>
2025-06-17 17:51:06 -0400

    first commit

---
+++ UI/ImageOneViewUserControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using Kitware.VTK;
+
+namespace esapi.UI
+{
+    public class ImageOneViewUserControl : UserControl
+    {
+        private RenderWindowControl renderWindowControl;
+
+        public ImageOneViewUserControl()
+        {
+            renderWindowControl = new RenderWindowControl
+            {
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(renderWindowControl);
+
+            renderWindowControl.Load += (_, __) => InitializeVTK();
+        }
+
+        private void InitializeVTK()
+        {
+            vtkSphereSource sphere = vtkSphereSource.New();
+            vtkPolyDataMapper mapper = vtkPolyDataMapper.New();
+            mapper.SetInputConnection(sphere.GetOutputPort());
+
+            vtkActor actor = vtkActor.New();
+            actor.SetMapper(mapper);
+
+            vtkRenderer renderer = renderWindowControl.RenderWindow.GetRenderers().GetFirstRenderer();
+            renderer.AddActor(actor);
+            renderer.SetBackground(0.1, 0.2, 0.4);
+            renderWindowControl.RenderWindow.Render();
+        }
+    }
+
+}
---
+++ UI/PatientControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using VMS.TPS.Common.Model.API;
+using esapi.ViewModel;
+
+namespace esapi.UI
+{
+    public partial class PatientControl : UserControl
+    {
+        public PatientControl()
+        {
+            InitializeComponent();
+        }
+
+        public void SetPatient(Patient patient)
+        {
+            //MyPropertyGrid.SelectedObject = new PatientViewModel(patient);
+        }
+    }
+}
